f5210b79c6fa32505b9477250d3095fddcb1bb1c
zverevaamaria <zverevaamaria@yandex.ru>
2021-03-25 19:03:39 +0300

    project is ready

---
+++ app.js
@@ -0,0 +1,66 @@
+const APP = () =>{
+  const song = document.querySelector('.sound');
+  const play = document.querySelector('.play');
+  const timeSelect = document.querySelectorAll('.time-select button');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
+  const sounds = document.querySelectorAll('.sound-picker button')
+  const timeDisplay = document.querySelector('.player-container h3');
+  const outlineLenght = outline.getTotalLength();
+  let fakeDuration = 120;
+
+  outline.style.strokeDasharray = outlineLenght;
+  outline.style.strokeDashoffset = outlineLenght;
+
+  sounds.forEach(sound => {
+    sound.addEventListener('click', function(){
+      song.src = this.getAttribute('data-sound');
+      video.src = this.getAttribute('data-video');
+      checkPlaying(song);
+    });
+  });
+
+
+  play.addEventListener('click', () => {
+    checkPlaying(song);
+  });
+
+  timeSelect.forEach(option => {
+    option.addEventListener('click', function(){
+      fakeDuration = this.getAttribute('data-time');
+      timeDisplay.textContent = `${ Math.floor(fakeDuration / 60)}:${ Math.floor(fakeDuration % 60)}`;
+    });
+  })
+
+   const checkPlaying = (song) =>{
+  if(song.paused){
+    song.play();
+    video.play();
+    play.src = './svg/pause.svg';
+  } else{
+    song.pause();
+    video.pause();
+    play.src = './svg/play.svg'
+  }
+};
+
+song.ontimeupdate = () => {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes =  Math.floor(elapsed / 60);
+
+  let progress = outlineLenght - (currentTime / fakeDuration) * outlineLenght;
+  outline.style.strokeDashoffset = progress;
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+
+  if(currentTime >= fakeDuration){
+    song.pause();
+    song.currentTime = 0;
+    play.src = './svg/play.svg';
+    video.pause();
+  }
+}
+
+};
+APP();
